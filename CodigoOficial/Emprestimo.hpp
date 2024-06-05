794e292a3cd8dc917690b9dba4d72d11de306af1
Adriele <adrieleestefanesantos@gmail.com>
2024-06-05 11:30:22 -0300

    new file:   CodigoOficial/CadastroAdministrador.cpp 	new file:   CodigoOficial/CadastroAdministrador.hpp 	new file:   CodigoOficial/CadastroAluno.cpp 	new file:   CodigoOficial/CadastroAluno.hpp 	new file:   CodigoOficial/CadastroLivro.hpp 	new file:   CodigoOficial/Emprestimo.cpp 	new file:   CodigoOficial/Emprestimo.hpp 	new file:   CodigoOficial/Livro.hpp 	new file:   CodigoOficial/LoginAdministrador.cpp 	new file:   CodigoOficial/LoginAdministrador.hpp 	new file:   CodigoOficial/LoginAluno.hpp 	new file:   CodigoOficial/administradores.txt 	new file:   CodigoOficial/devolver.cpp 	new file:   CodigoOficial/devolver.hpp 	new file:   CodigoOficial/main.cpp

---
+++ CodigoOficial/Emprestimo.hpp
@@ -0,0 +1,22 @@
+//Emprestimo.hpp
+#ifndef EMPRESTIMO_HPP
+#define EMPRESTIMO_HPP
+
+#include "Livro.hpp"
+#include "CadastroAluno.hpp"
+#include <string>
+
+class Emprestimo {
+public:
+    Emprestimo(Livro& livro, Aluno& aluno, const std::string& dataEmprestimo);
+    bool isLivroEmprestado();
+    void realizarEmprestimo();
+    Livro& getLivro();
+
+private:
+    Livro& livro;
+    Aluno& aluno;
+    std::string dataEmprestimo;
+};
+
+#endif
