8d401125be50931d0b60552b9400f5357cb5785e
Adriele <118401626+adriele07@users.noreply.github.com>
2024-07-06 15:53:50 -0300

    Update devolver.cpp

--- biblioteca/devolver.cpp
+++ biblioteca/devolver.cpp
@@ -18,19 +18,19 @@ void Devolver::realizarDevolucao() {
         std::stringstream ss(linha);
         std::string idLivro, titulo, autor, ano, nomeUsuario, numeroMatricula, dataEmprestimo;
         std::getline(ss, idLivro, ',');  // ID do livro
-        std::getline(ss, titulo, ',');   // Título do livro (ignorado)
+        std::getline(ss, titulo, ',');   // TÃ­tulo do livro (ignorado)
         std::getline(ss, autor, ',');    // Autor do livro (ignorado)
         std::getline(ss, ano, ',');      // Ano do livro (ignorado)
-        std::getline(ss, nomeUsuario, ','); // Nome de usuário do aluno
-        std::getline(ss, numeroMatricula, ','); // Número de matrícula (ignorado)
-        std::getline(ss, dataEmprestimo); // Data de empréstimo (ignorada)
+        std::getline(ss, nomeUsuario, ','); // Nome de usuÃ¡rio do aluno
+        std::getline(ss, numeroMatricula, ','); // NÃºmero de matrÃ­cula (ignorado)
+        std::getline(ss, dataEmprestimo); // Data de emprÃ©stimo (ignorada)
 
         // Verifica se a linha corresponde ao livro e ao aluno
         if (std::stoi(idLivro) == emprestimo.getLivro().getId() && nomeUsuario == emprestimo.getAluno().getNomeDeUsuario()) {
             livroEncontrado = true; // Livro emprestado pelo aluno encontrado
         }
         else {
-            temp << linha << "\n"; // Copia a linha para o arquivo temporário se não for o livro a ser devolvido
+            temp << linha << "\n"; // Copia a linha para o arquivo temporÃ¡rio se nÃ£o for o livro a ser devolvido
         }
     }
 
@@ -39,14 +39,14 @@ void Devolver::realizarDevolucao() {
     temp.close();
 
     if (livroEncontrado) {
-        // Deletar o arquivo original e renomear o arquivo temporário
+        // Deletar o arquivo original e renomear o arquivo temporÃ¡rio
         remove("emprestimos.txt");
         rename("temp.txt", "emprestimos.txt");
         std::cout << "Livro devolvido com sucesso." << std::endl;
     }
     else {
-        std::cerr << "Erro: Livro não está emprestado por este aluno." << std::endl;
-        // Deleta o arquivo temporário, pois a devolução não foi realizada
+        std::cerr << "Erro: Livro nao esta emprestado por este aluno." << std::endl;
+        // Deleta o arquivo temporÃ¡rio, pois a devoluÃ§Ã£o nÃ£o foi realizada
         remove("temp.txt");
     }
 }
